e12da4183bfe9669316513bdc1e74039044b0e33
Amogh-2404 <ramogh2404@gmail.com>
2024-07-14 21:20:05 +0530

    Conversational loop

--- main.py
+++ main.py
@@ -8,13 +8,29 @@ from langchain.chains import LLMChain, ConversationChain
 # from langchain_community.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
 
 
 load_dotenv()
 
 chat = ChatOpenAI()
-messages = [
-    SystemMessage(content="You are a helpful assistant that help the user to plan an optimized itinerary."),
-    HumanMessage(content="I'm going to Rome for 2 days, what can I visit?")]
-output = chat.invoke(messages)
-print(output.content)
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=chat, verbose=True, memory=memory)
+
+#
+# messages = [
+#     SystemMessage(content="You are a helpful assistant that help the user to plan an optimized itinerary."),
+#     HumanMessage(content="I'm going to Rome for 2 days, what can I visit?")]
+# output = chat.invoke(messages)
+# print(output.content)
+
+while True:
+    query = input("you: ")
+    if query == 'q':
+        break
+    output = conversation({"input": query})
+    print("User: ", query)
+    print('AI system: ', output['response'])
+
+
