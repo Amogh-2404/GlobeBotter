e08c9a42239c8a2cf1592ac642fd0518d76ed8da
Amogh-2404 <ramogh2404@gmail.com>
2024-07-14 23:04:04 +0530

    Added streamlit support

--- main.py
+++ main.py
@@ -1,36 +1,83 @@
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from langchain.chains import LLMChain, ConversationChain
-# from langchain.chat_models import ChatOpenAI
-# from langchain_community.chat_models import ChatOpenAI
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
+import streamlit as st
+from langchain.agents.agent_toolkits import create_conversational_retrieval_agent
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.utilities import SerpAPIWrapper
+from langchain_community.vectorstores import FAISS
+from langchain_core.tools import Tool, create_retriever_tool
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.callbacks import StreamlitCallbackHandler
 
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import ChatMessage
+from langchain_openai import ChatOpenAI
+import streamlit as st
+from dotenv import load_dotenv
 
 load_dotenv()
-
-chat = ChatOpenAI()
-memory = ConversationBufferMemory()
-conversation = ConversationChain(llm=chat, verbose=True, memory=memory)
-
-#
-# messages = [
-#     SystemMessage(content="You are a helpful assistant that help the user to plan an optimized itinerary."),
-#     HumanMessage(content="I'm going to Rome for 2 days, what can I visit?")]
-# output = chat.invoke(messages)
-# print(output.content)
-
-while True:
-    query = input("you: ")
-    if query == 'q':
-        break
-    output = conversation({"input": query})
-    print("User: ", query)
-    print('AI system: ', output['response'])
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text=""):
+        self.container = container
+        self.text = initial_text
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
 
 
+st.set_page_config(page_title="GlobeBotter", page_icon="🌐")
+st.header(
+    '🌐 Welcome to Globebotter, your travel assistant with Internet access. What are you planning for your next trip?')
+
+search = SerpAPIWrapper()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1500,
+    chunk_overlap=200
+)
+raw_documents = PyPDFLoader('italy_travel.pdf').load()
+documents = text_splitter.split_documents(raw_documents)
+db = FAISS.from_documents(documents, OpenAIEmbeddings())
+memory = ConversationBufferMemory(
+    return_messages=True,
+    memory_key="chat_history",
+    output_key="output"
+)
+llm = ChatOpenAI()
+tools = [
+    Tool.from_function(
+        func=search.run,
+        name="Search",
+        description="useful for when you need to answer questions about current events"
+    ),
+    create_retriever_tool(
+        db.as_retriever(),
+        "italy_travel",
+        "Searches and returns documents regarding Italy."
+    )
+]
+agent = create_conversational_retrieval_agent(llm, tools, memory_key='chat_history', verbose=True)
+
+user_query = st.text_input(
+    "**Where are you planning your next vacation?**",
+    placeholder="Ask me anything!"
+)
+
+if 'messages' not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", 'content': 'How can I help you?'}]
+if "memory" not in st.session_state:
+    st.session_state["memory"] = memory
+
+for msg in st.session_state['messages']:
+    st.chat_message(msg["role"]).write(msg['content'])
+
+if user_query:
+    st.session_state.messages.append({'role': 'user', "content": user_query})
+    st.chat_message('user').write(user_query)
+    with st.chat_message('assistant'):
+        stream_handler = StreamHandler(st.empty())
+        llm = ChatOpenAI(streaming=True, callbacks=[stream_handler])
+        response = llm.invoke(st.session_state.messages)
+        st.session_state.messages.append((ChatMessage(role="assistant", content=response.content)))
+
+if st.sidebar.button("Reset chat history"):
+    st.session_state.messages = []
